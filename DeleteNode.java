8a94a8d4327b8aa623e2162d88120695eeb6100e
Mounika8300 <88379577+Mounika8300@users.noreply.github.com>
2025-06-08 03:16:32 -0500

    Update DeleteNode.java

--- DeleteNode.java
+++ DeleteNode.java
@@ -1,3 +1,8 @@
+// Time complexity - O(1)
+// Space Complexity - O(1)
+// Did you face any issues - No
+// Solved on leetcode - yes
+// Simply updating the node next value
 public void deleteNode(ListNode node) {
     node.val = node.next.val;
     node.next = node.next.next;
